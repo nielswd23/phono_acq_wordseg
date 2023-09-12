2346815acc8090d3b828b6db57599846e53fa22b
nielswd23 <ndicks3@lsu.edu>
2023-09-12 14:40:03 -0700

    initial setup for phonotactic models and reformating wordseg input

---
+++ phonotactic_models.py
@@ -0,0 +1,5 @@
+import ngram_calculator.py 
+
+ngram_calculator.run(train, test, out)
+# path to training (list of words), path to test (list of words), 
+# path to out (output file, i think can be empty it just needs a plce to put results)
